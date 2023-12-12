543808c9b17f36f38ac9f2c81666fbae06068061
Karas <maciej2.karaskiewicz@gmail.com>
2023-12-12 10:41:01 +0100

    Day 12 part 2 completed

--- src/12/12_2.py
+++ src/12/12_2.py
@@ -5,6 +5,6 @@ data = parse_springs_from_input("../../Inputs/12")
 
 all_solutions = 0
 for springs, numbers in data:
-    all_solutions += number_of_solutions(springs+".", numbers)
+    all_solutions += number_of_solutions("?".join([springs]*5)+".", numbers*5)
 
 print(all_solutions)
--- src/12/utils.py
+++ src/12/utils.py
@@ -1,3 +1,4 @@
+from functools import cache
 from src.utils import load_input
 
 
@@ -12,3 +13,21 @@ def parse_springs_from_input(path):
     return list(map(parse_spring_set, lines))
 
 
+@cache
+def number_of_solutions(springs_array: str, sizes: tuple[int, ...], num_in_group=0):
+    if not springs_array:
+        return not sizes and not num_in_group
+
+    solutions = 0
+    possible = [".", "#"] if springs_array[0] == "?" else springs_array[0]
+
+    for c in possible:
+        if c == "#":
+            solutions += number_of_solutions(springs_array[1:], sizes, num_in_group+1)
+        else:
+            if num_in_group:
+                if sizes and sizes[0] == num_in_group:
+                    solutions += number_of_solutions(springs_array[1:], sizes[1:])
+            else:
+                solutions += number_of_solutions(springs_array[1:], sizes)
+    return solutions
